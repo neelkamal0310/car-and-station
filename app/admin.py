f39d05ba1b97a16a136f0704a1f4789be3a56a34
Neel <nk10399@gmail.com>
2021-11-11 21:38:08 +0530

    add change station

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from .models import Station, Vehicle
 
 # Register your models here.
@@ -7,14 +8,19 @@ from .models import Station, Vehicle
 class VehicleAdmin(admin.ModelAdmin):
     list_display = ['regdno', 'station', 'fuel', 'odometer', 'booked']
     ordering = ['regdno']
-    actions = ['mark_vehicles_booked', 'mark_vehicles_unbooked']
+    actions = ['mark_vehicles_booked', 'mark_vehicles_unbooked', 'change_station']
 
     def mark_vehicles_booked(self, request, queryset):
         queryset.update(booked=True)
+        print(queryset.values_list('pk', flat=True))
 
     def mark_vehicles_unbooked(self, request, queryset):
         queryset.update(booked=False)
 
+    def change_station(self, request, queryset):
+        selected = queryset.values_list('pk', flat=True)
+        ids = ','.join(str(pk) for pk in selected)
+        return HttpResponseRedirect(f'/changeStation/?ids={ids}')
 
 class StationAdmin(admin.ModelAdmin):
     list_display = ['name', 'state', 'vehicles']
--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from .models import Station, Vehicle
+
 # Create your views here.
+
+
+def changeStation(request):
+    if not request.GET.get('ids'):
+        return HttpResponse('')
+    if request.method == 'POST':
+        vehicles = request.GET.get('ids', None)
+        pks = vehicles.split(',')
+        station_id = int(request.POST.get('station'))
+        for pk in pks:
+            obj = Vehicle.objects.get(pk=pk)
+            obj.station = Station.objects.get(pk=station_id)
+            obj.save()
+        return HttpResponseRedirect('/admin/app/vehicle/')
+    stations = Station.objects.all()
+    return render(request, 'ind.html', {'stations': stations})
